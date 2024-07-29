36681e7465134ca692ae6d0df8b40994a0e2a3e9
Nick Turner <ncttcn@gmail.com>
2024-07-29 16:31:08 -0500

    ep 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+
+        <p>{10*9}</p>
       </div>
     </div>
   );
