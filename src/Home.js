1d5dca03f9751309c836e242c265b244751c6dc0
Nick Turner <ncttcn@gmail.com>
2024-07-30 17:19:25 -0500

    bunch

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+    ])
 
-    const handleClick = (e) => {
-        console.log("Hello",e)
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log("Use Effect Ran!");
+    });
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+           <BlogList blogs = {blogs} title = "All Blogs!" handleDelete={handleDelete}></BlogList>
+           
         </div>
      );
 }
