8d19e55a0ffb1d1d4f167ea62fb47fda7f1ef2d6
Nick Turner <ncttcn@gmail.com>
2024-08-07 13:56:57 -0500

    finished react tut

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Navbar from './NavBar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home/>}></Route>
+            <Route path='/create' element={<Create/>}></Route>
+            <Route path='/blogs/:id' element={<BlogDetails/>}></Route>
+            <Route path = '*' element={<NotFound/>}></Route>
+            
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
